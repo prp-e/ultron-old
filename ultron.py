cca15a4bbad250c31db1eb4d25fc5303051c74b7
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-01-28 19:31:42 +0330

    core: core of ultron

---
+++ ultron.py
@@ -0,0 +1,9 @@
+import ktrain
+from ktrain import text 
+
+INDEXDIR = 'index_file'; 
+input_file = open('input_data.txt')
+input_data = [line for line in input_file.readlines()]
+
+text.SimpleQA.initialize_index(INDEXDIR)
+text.SimpleQA.index_from_list(input_data, INDEXDIR, commit_every=len(input_data), multisegment=True, procs=4, breakup_docs=True)
